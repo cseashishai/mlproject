0081114250c6668060e586351c8e8c4f2e87b52a
ashu <cseashishai.com>
2026-07-15 17:14:48 +0530

    EDA and Problem statment

--- src/logger.py
+++ src/logger.py
@@ -3,15 +3,15 @@ import os # WHich operating system
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+logs_path=os.path.join(os.getcwd(),"logs")
 os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH =os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
+    filename=LOG_FILE_PATH,  ## where To Save 
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s -%(message)s",
-    level=logging.INFO,
+    level=logging.INFO, # Which log save
 
 )
 
